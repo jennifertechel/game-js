bb08b234b4f4cd8334f698a0a3bb58f90b404af3
Jennifer Techel <jennifertechel@hotmail.com>
2022-11-21 13:03:19 +0100

    Added username-function

--- main.js
+++ main.js
@@ -2,32 +2,44 @@ window.addEventListener('DOMContentLoaded', main);
 
 function main() {
     loadStartScene();
+    createHtmlElements();
 }
+
+const h1 = document.createElement("h1");
+const p = document.createElement("p");
+const input = document.createElement("input");
+const button1 = document.createElement("button");
+const button2 = document.createElement("button");
+
+function createHtmlElements() {
+}
+
+let username = "";
 
 function loadStartScene() {
 
-    const h1 = document.createElement("h1");
-    const p = document.createElement("p");
-    const button1 = document.createElement("button");
-
     h1.textContent = "Welcome";
     p.textContent = "Oh no, we have a big problem. You overslept and have to get to that important meeting in asap. Before we begin, enter your name."
+    input.type = "text";
+    input.placeholder = "Enter your name";
     button1.textContent = "Let's Begin!";
-    button1.onclick = loadTransportScene;
+    button1.onclick = saveUsername;
     
     const container = document.getElementById("container");
     container.innerHTML = "";
-    container.append(h1, p, button1);
+    container.append(h1, p, input, button1);
+}
+
+function saveUsername() {
+    username = input.value;
+    input.value = "";
+    loadTransportScene();
+    console.log(username);
 }
 
 function loadTransportScene() {
     
-    const h1 = document.createElement("h1");
-    const p = document.createElement("p");
-    const button1 = document.createElement("button");
-    const button2 = document.createElement("button");
-
-    h1.textContent = "First Scene";
+    h1.textContent = "Hello " + username;
     p.textContent = "You get dressed in a hurry. You leave the apartment. Should you run to the bus or call a taxi?"
     button1.textContent = "Run to the bus";
     button1.onclick = loadCoffeeScene;
@@ -43,28 +55,24 @@ function loadTransportScene() {
 
 function loadCoffeeScene() {
 
-    const h1 = document.createElement("h1");
-    const p = document.createElement("p");
-    const button1 = document.createElement("button");
-    const button2 = document.createElement("button");
-
     h1.textContent = "Yay!";
     p.textContent = "You caught the bus just in time. But getting on i a rush results in bumping into a stranger and their coffee spilling all over you. Your shirt is now ruined. Should you make another stop to get a new one, or just button your blazer and hope no one notices?";
     button1.textContent = "Buy a new one";
     button1.onclick = loadShirtScene;
   
     button2.textContent = "Hope no one notices";
+    button2.onclick = loadOfficeScene;
 
     const container = document.getElementById("container");
     container.innerHTML = "";
     container.append(h1, p, button1, button2);
 }
 
-function loadToiletScene() {
+function loadOfficeScene() {
+    console.log("Office Scene");
+}
 
-    const h1 = document.createElement("h1");
-    const p = document.createElement("p");
-    const button1 = document.createElement("button");
+function loadToiletScene() {
 
     h1.textContent = "Oh no.."
     p.textContent = "Woops. In all the haste you drop your phone in the toilet, with all the important information for the meeting. You have no choice but to admit defeat. This day is not happening.";
@@ -77,10 +85,6 @@ function loadToiletScene() {
 }
 
 function loadShirtScene() {
-
-    const h1 = document.createElement("h1");
-    const p = document.createElement("p");
-    const button1 = document.createElement("button");
 
     h1.textContent = "Almost..."
     p.textContent = "You bought and new one and changed in the office bathroom. But you are now two hours late, the meeting is over and your boss fires you on the spot.";
